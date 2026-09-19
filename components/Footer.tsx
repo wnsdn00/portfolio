975d4e0fcd6230f13b1f9960644d5614be9646ed
wnsdn00 <114475584+wnsdn00@users.noreply.github.com>
2026-09-19 22:19:55 +0900

    Update Footer component with additional contact info
    Added a phone contact to the footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,8 @@ export default function Footer() {
   return (
     <footer className="py-12 text-center text-zinc-500">
       이 외에 궁금하신 부분은 아래로 연락주시면 감사합니다.<br/>
-      E-mail: junwoo000706@gmail.com
+      E-mail: junwoo000706@gmail.com<br/>
+      call: 010-9947-5967
     </footer>
   );
 }
